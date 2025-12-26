9844695468bd8a050ebf445a114696f58a5da3a8
Eduardo Braz de Sá <eduardobraz@MacBook-Pro-de-Eduardo.local>
2025-12-25 21:43:19 -0300

    Ativando modo estatico export

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,10 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: 'export',
+  images: {
+    unoptimized: true, // Necessário para sites estáticos
+  },
 };
 
 export default nextConfig;
